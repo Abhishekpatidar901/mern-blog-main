8a13206bc1be64096ec4ce60ef6d44f16ebccb62
Abhishekpatidar901 <abhishekpatidar901@gmail.com>
2024-04-21 14:07:49 +0530

    Mern-Blog-application

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9_63tR92c_oN2CrxD3zrDEYCl46EIPXw",
+  authDomain: "mern-blog-996af.firebaseapp.com",
+  projectId: "mern-blog-996af",
+  storageBucket: "mern-blog-996af.appspot.com",
+  messagingSenderId: "122823297784",
+  appId: "1:122823297784:web:a3ae8f21ac0a3b5652890d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
